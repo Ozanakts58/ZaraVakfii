1d01c1377626494c8afbec713cd43706f334f829
Ozan Aktaş <2016123050@cumhuriyet.edu.tr>
2024-06-11 16:04:42 +0300

    commit change request

--- lib/Screen/Body/body_parts_slider.dart
+++ lib/Screen/Body/body_parts_slider.dart
@@ -12,13 +12,13 @@ class BodyPartsSlider extends StatefulWidget {
 }
 
 class _BodyPartsSliderState extends State<BodyPartsSlider> {
-  final urlImages = [/*
+  final urlImages = [
     'https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/439246693_736845115314366_1836867855229520569_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=K_He2GXABCMQ7kNvgFNC04L&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYAqZg_qkjt9QCHHdyIIgk6J5pd4osOQ0pEDQTF7ezLJ0A&oe=66678DFD',
     'https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/438299938_736845295314348_3531102844214222509_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=6DHAj6p_ZwcQ7kNvgFf-qko&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYDPpT6aAkKFIAdW4fT2-xWFrVFzgLvFZ4LRzVTiGsk8kg&oe=66679295',
     'https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/436410976_720628040269407_1118398702530632342_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_JkSSlugtHgQ7kNvgFsiOVl&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYDMPfMYvM5uyLb33dscStL0mjTMbA7YRMKqDylo2L3bcA&oe=66679CC5',
     'https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/434561014_716494604016084_5381749719750788898_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=usjNy6p6PIoQ7kNvgHQi-mK&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYCMtaAdXKHgbnOuktPGeoW2dZstr27_jZda1sxi2mz1Ig&oe=6667976E',
     'https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/434240932_715321234133421_6944283839686068408_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gcZRHdKtVfoQ7kNvgFiJzw7&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYA_I4mGG8TSz56Uo_9ICYStmffXKIuLgPwBTQiK7WgATw&oe=6667A71B',
-  */];
+  ];
 
   @override
   Widget build(BuildContext context) {
--- lib/constants/values.dart
+++ lib/constants/values.dart
@@ -8,19 +8,19 @@ class StaticValues {
         descrption:
             "2 Mart Cumartesi günü Zara’da tüm Türkiye’ye örnek olacak kahvaltılı bir toplantı gerçekleştirildi. Zara Vakfı, Zasiad, ilçemizin 5 federasyonu (Kaykef, Kıydef, Özfed, Şedef, Zadef), Ankara Zaralılar Derneği, Sivas Zaralılar Derneği, Yeşil Zara Derneği olmak üzere yöremize ait tüm STK’ların başkan ve temsilcileri, Zara Müftüsü, Zara Cemevi Başkanı, Zara İlçe Emniyet Müdürü, Zara Jandarma Komutanı, ilçemizde bulunan tüm odaların başkanları, ilçemizde bulunan tüm siyasi partilerin ilçe başkanları ile 31 Mart Yerel seçimlerinde ilçemizde yarışacak 5 aday Beraberlik Kahvaltısı’nda bir araya geldiler.- Zara Vakfı Yönetim Kurulu",
         image:
-            "",/*"https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/429536538_700382022294009_1764881795374736396_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=TWl9vfXiOAYQ7kNvgECuT0O&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYC4w79TE-vx4UL8dXqUoxoRgNvrznrcTqZKaRWJqT8jEg&oe=66678A7C",*/
+            "https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/429536538_700382022294009_1764881795374736396_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=TWl9vfXiOAYQ7kNvgECuT0O&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYC4w79TE-vx4UL8dXqUoxoRgNvrznrcTqZKaRWJqT8jEg&oe=66678A7C",
         source: "Zara Vakfı Yönetim Kurulu",
         sourceImage:
-            "",/*"""https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",*/
+            "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",
         time: "02/03/2024"),
     News(
       title:
           "Tekrar Zara Belediye Başkanı Olarak Seçilen Sayın Fatih Çelik’i Ziyaret",
       image:
-          "",/*"""https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/434470447_716156370716574_8169162569681572454_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gWCXWg182mgQ7kNvgFJITHb&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYDuKA7Wb5OsmuqYxnHh8u2n_zJFPBlEvXoGMCM_Slc_4g&oe=6667951A",*/
+          "https://scontent.fsaw2-2.fna.fbcdn.net/v/t39.30808-6/434470447_716156370716574_8169162569681572454_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gWCXWg182mgQ7kNvgFJITHb&_nc_ht=scontent.fsaw2-2.fna&oh=00_AYDuKA7Wb5OsmuqYxnHh8u2n_zJFPBlEvXoGMCM_Slc_4g&oe=6667951A",
       source: "Zara Vakfı Yönetim Kurulu",
       sourceImage:
-          "",/*"https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",*/
+          "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",
       time: "02/01/2024",
       descrption:
           "Vakıf başkan vekilimiz Av. Aykut Bayrakcı, 31 Mart yerel seçimlerinde tekrar Zara Belediye Başkanı olarak seçilen Sayın Fatih Çelik’i ziyaret ederek, Zara Vakfı adına tebriklerini iletti. Başkan Çelik; yeni dönemde de başta Zara Vakfı olmak üzere tüm STK’lar ile iş birliği içerisinde çalışmaya devam edeceğini belirterek, ortak gayenin Zara’ya ve Zara İnsanına hizmet etmek olduğunu belirtti. Biz de bir kez daha kendisini tebrik ediyor, zorlu görevinde kolaylıklar diliyoruz. - Zara Vakfı Yönetim Kurulu",
@@ -28,10 +28,10 @@ class StaticValues {
     News(
         title: "Kızılırmak Yöresi Dernekler Federasyonu 4. Olağan Genel Kurulu gerçekleştirildi.r",
         image:
-            "",/*"""https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/434192437_715320810800130_596177021888088811_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Yq2jQGwtyMUQ7kNvgGlrmD6&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYDVErBpaiZI_EXTMHppqXBVeLnq4wMyBre4PnLlDRknTw&oe=66679CDB",*/
+            "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/434192437_715320810800130_596177021888088811_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Yq2jQGwtyMUQ7kNvgGlrmD6&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYDVErBpaiZI_EXTMHppqXBVeLnq4wMyBre4PnLlDRknTw&oe=66679CDB",
         source: "Zara Vakfı Yönetim Kurulu",
         sourceImage:
-            "",/*"https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",*/
+            "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",
         time: "02/08/2023",
         descrption:
             "31.03.2024 günü Bozkır Köyü dernek lokalinde, iftar öncesi Kızılırmak Yöresi Dernekler Federasyonu 4. Olağan Genel Kurulu gerçekleştirildi. Divan başkanlığını vakıf başkanımız Eyüp Bayrak’ın yaptığı, Vakıf başkan vekilimiz Av. Aykut Bayrakcı, yönetim kurulu üyemiz Osman Nuri Özkan ve Zasiad Başkanı Sayın Ufuk Rufai Şimşek ile federasyon üyesi dernek temsilcilerinin katıldığı genel kurul sonrası, mevcut başkan Mehmet Bağca tekrar başkan olarak seçildi. Kendisini ve yönetimini tebrik ediyor, başarılı hizmetlerinin devamını diliyoruz. - Zara Vakfı Yönetim Kurulu",),
@@ -39,10 +39,10 @@ class StaticValues {
       title:
       "Sultangazi Sivaslılar Derneği tarafından Sivaslılar iftar programı düzenlendi. ",
       image:
-      "",/*"https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/434240932_715321234133421_6944283839686068408_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gcZRHdKtVfoQ7kNvgFiJzw7&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYA_I4mGG8TSz56Uo_9ICYStmffXKIuLgPwBTQiK7WgATw&oe=6667A71B",*/
+      "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/434240932_715321234133421_6944283839686068408_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gcZRHdKtVfoQ7kNvgFiJzw7&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYA_I4mGG8TSz56Uo_9ICYStmffXKIuLgPwBTQiK7WgATw&oe=6667A71B",
       source: "Zara Vakfı Yönetim Kurulu",
       sourceImage:
-      "",/*"""https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",*/
+      "https://scontent.fsaw2-3.fna.fbcdn.net/v/t39.30808-6/348229039_811607356577126_7901569625488659994_n.png?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-Gw7Dy0NX1sQ7kNvgFqQzM-&_nc_ht=scontent.fsaw2-3.fna&oh=00_AYC_Y5JU_t1cJPWOy9itb68j4h-WBl36BXQ0z1InByT5yw&oe=6667A095",
       time: "02/07/2023",
       descrption:
       "Vakıf başkanımız Eyüp Bayrak’ın başkanlığını yürüttüğü Sultangazi Sivaslılar Derneği tarafından Sivaslılar iftar programı düzenlendi. Hemşehrilerimizin yoğun ilgi gösterdiği iftara, İstanbul milletvekili Sivaslı hemşehrimiz Av. Adem Yıldırım ve Sultangazi Belediye Başkanı Av. Abdurrahman Dursun, belediye başkan yardımcısı Zaralı hemşehrimiz Av. Fatih Sultan Mehmet Çiçek de katılım sağladı. İftar sonrası Zaralı hemşehrilerimiz, etkinlik alanında kurulan Sivas standını ziyaret etti. Böylesi kalabalık bir organizasyonu kusursuz bir şekilde düzenleyen ve hemşehrilerimizi bir araya getiren Sultangazi Sivaslılar Derneği Başkan ve yönetimini tebrik ediyor, başarılı hizmetlerinin devamını diliyoruz. - Zara Vakfı Yönetim Kurulu",
